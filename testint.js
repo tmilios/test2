cad976991b214ba7d9b137628cc262e79f1dc7ea
tmilios <tmilios7@gmail.com>
2016-11-08 11:20:24 -0500

    tesr3
    terter

---
+++ testint.js
@@ -0,0 +1,4 @@
+habricator supports two similar but separate code review workflows:
+
+Differential is used for pre-push code review, called "reviews" elsewhere in the documentation. You can learn more in Differential User Guide.
+Audit is used for post-push code reviews, called "audits" elsewhere in the documentation. You can learn more in Audit User Guide.
